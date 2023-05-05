c567240b85bbd9c60449bce47f2b47a8c75c72fd
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-05 10:52:55 -0300

    resolvendo lint

--- src/teste.js
+++
@@ -1,9 +0,0 @@
-function verificaFimPalavra(palavra1, palavra2) {
-  if (palavra1.endsWith(palavra2)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-verificaFimPalavra('trybe', 'be');
