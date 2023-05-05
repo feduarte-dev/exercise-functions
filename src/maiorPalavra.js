4f668f4a498c836b35e5aeeb232af653f3bf4823
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-05 10:54:51 -0300

    resolvendo lint2

---
+++ src/maiorPalavra.js
@@ -0,0 +1,11 @@
+function maiorPalavra(array) {
+  let palavra = '';
+  for (index = 0; index <= array.length; index += 1) {
+    if (array[index].length > array[index + 1].length) {
+      palavra = array[index];
+    }
+  }
+  return palavra;
+}
+maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']);
+console.log(palavra)
