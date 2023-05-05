789869400fd205b3a959ef91717c9461be29e7cb
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-05 10:21:27 -0300

    requisito 6 completo

--- src/functions.js
+++ src/functions.js
@@ -64,7 +64,18 @@ function repeticoes(repetido) {
 }
 
 // Requisito 6 - Crie a função somatorio
-
+function somatorio(n) {
+  let sum = 0;
+  if (n < 0) {
+    return 'ERRO';
+  }
+  if (n > 0) {
+    for (let index = 0; index <= n; index += 1) {
+      sum += index;
+    }
+    return sum;
+  }
+}
 // Requisito 7 - Crie a função verificaFimPalavra
 
 // Não modifique essas linhas
--- src/teste.js
+++ src/teste.js
@@ -1,23 +1,13 @@
-function maisRepetido(array) {
-  let repetido = array[0];
-
-  for (index = 0; index < array.length; index += 1) {
-    repetido = 0;
-    for (index2 = 0; index2 < array.length; index2 += 1) {
-      if (array[index2] === array[index]) {
-        repetido += 1;
-      }
+function somatorio(n) {
+  let sum = 0;
+  if (n < 0) {
+    return 'ERRO';
+  }
+  if (n > 0) {
+    for (let index = 0; index <= n; index += 1) {
+      sum += index;
     }
-  }
-  console.log(repetido);
-  return repetido;
-}
-function repeticoes(repetido) {
-  let repeticoes = 0;
-  if (repetido > repeticoes) {
-    repeticoes = repetido;
-    repetido = array[index];
+    return sum;
   }
 }
-
-maisRepetido([2, 3, -2, 3, -2, 2, 3]);
+somatorio(3);
